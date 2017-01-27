00a952f4febf614623c1cbc65aeaa213ed9ba72f
Bradley Falzon <brad@teambrad.net>
2017-01-27 16:38:07 +1030

    Add dominikh's gosimple, staticcheck and unused tools

---
+++ migrations/3_tools_dominikh.sql
@@ -0,0 +1,8 @@
+-- +migrate Up
+INSERT INTO tools (name, url, path, args, `regexp`) VALUES
+    ("gosimple", "https://github.com/dominikh/go-tools", "gosimple", "./...", ""),
+    ("staticcheck", "https://github.com/dominikh/go-tools", "staticcheck", "./...", ""),
+    ("unused", "https://github.com/dominikh/go-tools", "unused", "./...", "");
+
+-- +migrate Down
+
